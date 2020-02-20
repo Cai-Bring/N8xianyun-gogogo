d5e1f42a73b8ca16005f78c0b9861c4938855152
jack <jackjun8090@163.com>
2020-02-20 16:23:39 +0800

    面包屑功能

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,44 +1,62 @@
 <template>
-  <div class="hotel">
-    酒店页面
+  <div id="hotel">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>酒店</el-breadcrumb-item>
+      <el-breadcrumb-item>{{city}}酒店预定</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      usermaplocation: {}
-    }
+      usermaplocation: {},
+      city: ''
+    };
   },
-  mounted () {
+  mounted() {
     // IP定位代码
-    window.onLoad  = function(){
-      var map = new AMap.Map('container')
-      AMap.plugin('AMap.CitySearch', function () {
-        var citySearch = new AMap.CitySearch()
-        citySearch.getLocalCity(function (status, result) {
-          if (status === 'complete' && result.info === 'OK') {
-            // 查询成功，result即为当前所在城市信息
-            console.log(result);
-          }
-        })
-      })
-    }
-    var url = 'https://webapi.amap.com/maps?v=1.4.15&key=9623915ac82517f2e7ba4b95ef9ed725&callback=onLoad';
-    var jsapi = document.createElement('script');
-    jsapi.charset = 'utf-8';
-    jsapi.src = url;
-    document.head.appendChild(jsapi);
+    window.onLoad = () => {
+      var map = new AMap.Map("container");
+      this.maplocation()
+    };
+    this.init()
   },
   methods: {
-    maplocation () {
-      
+    maplocation() {
+      if (Object.keys(this.$route.query).length === 0) {
+        AMap.plugin("AMap.CitySearch", () => {
+        var citySearch = new AMap.CitySearch();
+        citySearch.getLocalCity((status, result) => {
+          if (status === "complete" && result.info === "OK") {
+            // 查询成功，result即为当前所在城市信息
+            this.$router.push({name:'hotel', query:{city: result.city}})
+          }
+        });
+      });
+      }
+    },
+    init () {
+      var url =
+      "https://webapi.amap.com/maps?v=1.4.15&key=9623915ac82517f2e7ba4b95ef9ed725&callback=onLoad";
+      var jsapi = document.createElement("script");
+      jsapi.charset = "utf-8";
+      jsapi.src = url;
+      document.head.appendChild(jsapi);
+    }
+  },
+  watch: {
+    '$route.query' () {
+      this.city = this.$route.query.city
     }
   }
-}
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+#hotel {
+  width: 1000px;
+  margin: 20px auto;
+}
 </style>
