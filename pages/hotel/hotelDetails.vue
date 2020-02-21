63496aa0a4dde751317dfe97f7aaec9732e03771
Cai-Bring <743489214@qq.com>
2020-02-21 18:05:47 +0800

    酒店详情页地图配置完成

--- pages/hotel/hotelDetails.vue
+++ pages/hotel/hotelDetails.vue
@@ -41,19 +41,34 @@
         </el-table-column>
       </el-table>
     </div>
+
+    <!-- 地图部分 -->
+    <el-row class="map" type="flex">
+      <el-col>
+        <div id="container"></div>
+      </el-col>
+      <el-col style="margin-left:20px">
+        <el-tabs v-model="activeName" @tab-click="handleClick" :stretch="true">
+          <el-tab-pane label="风景" name="first"></el-tab-pane>
+          <el-tab-pane label="交通" name="second"></el-tab-pane>
+        </el-tabs>
+        <div id="panel"></div>
+      </el-col>
+    </el-row>
     <!-- 底部评论部分 -->
     <div class="remarks">
       <!-- <el-rate v-model="value" disabled text-color="#ff9900" score-template="{value}"></el-rate> -->
     </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
     return {
       myhotel: {},
-      value: 0
+      value: 0,
+      map: "",
+      activeName: "first"
     };
   },
   mounted() {
@@ -65,7 +80,65 @@ export default {
       this.myhotel = res.data.data[0];
       this.value = res.data.data[0].stars;
       console.log(this.myhotel);
+      // 异步导入地图js （来自于官网）
+      var url =
+        "https://webapi.amap.com/maps?v=1.4.15&key=0e50af054087c7bec2bd57e4356a4bd3&callback=onLoad";
+      var jsapi = document.createElement("script");
+      jsapi.charset = "utf-8";
+      jsapi.src = url;
+      document.head.appendChild(jsapi);
+      // 地图初始化
+      window.onLoad = () => {
+        this.map = new AMap.Map("container", {
+          resizeEnable: true, //是否监控地图容器尺寸变化
+          zoom: 11, //初始化地图层级
+          center: [
+            this.myhotel.location.longitude,
+            this.myhotel.location.latitude
+          ]
+        });
+        this.searchmap("风景");
+      };
     });
+  },
+  methods: {
+    handleClick(tab, event) {
+      // console.log(tab);
+      // console.log(event);
+      this.map = new AMap.Map("container", {
+        resizeEnable: true, //是否监控地图容器尺寸变化
+        zoom: 11, //初始化地图层级
+        center: [
+          this.myhotel.location.longitude,
+          this.myhotel.location.latitude
+        ]
+      });
+      this.searchmap(tab.label);
+    },
+    searchmap(whereplace) {
+      AMap.service(["AMap.PlaceSearch"], () => {
+        //构造地点查询类
+        var placeSearch = new AMap.PlaceSearch({
+          pageSize: 10, // 单页显示结果条数
+          pageIndex: 1, // 页码
+          city: this.myhotel.real_city, // 兴趣点城市
+          citylimit: true, //是否强制限制在设置的城市内搜索
+          map: this.map, // 展现结果的地图实例
+          panel: "panel", // 结果列表将在此容器中进行展示。
+          autoFitView: true // 是否自动调整地图视野使绘制的 Marker点都处于视口的可见范围
+        });
+        var cpoint = [
+          this.myhotel.location.longitude,
+          this.myhotel.location.latitude
+        ]; //中心点坐标
+        placeSearch.searchNearBy(whereplace, cpoint, 1000, function(
+          status,
+          result
+        ) {
+          // console.log(result);
+        });
+      });
+    }
   }
 };
 </script>
@@ -95,11 +168,23 @@ export default {
     }
   }
   .hotelprice {
+    margin-bottom: 30px;
     /deep/.chearprice {
       color: #f90;
     }
     /deep/.el-icon-arrow-right {
       color: #f90;
+    }
+  }
+  .map {
+    #container {
+      width: 650px;
+      height: 360px;
+    }
+    #panel {
+      width: 100%;
+      height: 300px;
+      overflow: auto;
     }
   }
 }
