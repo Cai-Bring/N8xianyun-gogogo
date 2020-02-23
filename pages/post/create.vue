53d0917d04c5ec702558e3e4d8ed14a4a6fbf710
Cai-Bring <743489214@qq.com>
2020-02-23 18:37:09 +0800

    修复了顶部header的bug

---
+++ pages/post/create.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="create"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less">
+.create {
+  width: 1000px;
+  margin: 20px auto;
+}
+</style>
